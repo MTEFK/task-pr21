ee611ccc6528a2f6c0d1c93435cc33e6bf539330
TSShrol <48250029+TSShrol@users.noreply.github.com>
2025-02-14 00:36:42 +0200

    init and added tasks

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='block1'>        <a className='App-link' href='http://college.mlyniv.rv.ua/'>
+          МТЕФК
+        </a>
+        </div>
+        <div className='block2'>
+          <span>ГРУПА К41 </span>
+        </div>
+      </header>
+      <main>
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
